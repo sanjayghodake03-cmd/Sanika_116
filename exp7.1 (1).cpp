c07327f8b3a42d881fa63b9289884a6655504208
sanjayghodake03-cmd <sanjayghodake03@gmail.com>
2025-11-04 23:05:25 +0530

    Add files via upload

---
+++ exp7.1 (1).cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Distance {
+    cout << "\nFeet & Inches (After Decrement): " 
+             << feet << " feet " << inch << " inches" << endl;
+    }
+};
+
+int main() {
+    cout << "Name:sanika sanjay ghodke\n";
+    cout << "Roll No: 116\n";
+    cout << "Division: S.Y.B CSE\n";
+
+   
+    Distance d1(8, 9);
+    cout << "Initial Distance: " << d1.feet << " feet " << d1.inch << " inches" << endl;
+public:
+    int feet, inch;
+
+    Distance(int f, int i) {
+        this->feet = f;   
+        this->inch = i;
+    }
+
+    void operator-() {
+        feet--;
+        inch--;
+
+    -d1;  
+
+    return 0;
+}
+
+
